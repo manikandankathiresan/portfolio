a883eb56b335c9e25f5bc235d2ca94b99cf8c15d
Manikandan K <mkprofile98@gmail.com>
2024-03-06 09:30:13 +0530

    Points to be changed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { useContext } from 'react'
-import { ThemeContext } from './contexts/theme'
-import Header from './components/Header/Header'
+import './App.css'
 import About from './components/About/About'
-import Projects from './components/Projects/Projects'
-import Skills from './components/Skills/Skills'
-import ScrollToTop from './components/ScrollToTop/ScrollToTop'
 import Contact from './components/Contact/Contact'
 import Footer from './components/Footer/Footer'
-import './App.css'
+import Header from './components/Header/Header'
+import Projects from './components/Projects/Projects'
+import ScrollToTop from './components/ScrollToTop/ScrollToTop'
+import Skills from './components/Skills/Skills'
+import { ThemeContext } from './contexts/theme'
+// import Honeres from './components/Honors'
 
 const App = () => {
   const [{ themeName }] = useContext(ThemeContext)
@@ -20,6 +21,7 @@ const App = () => {
         <About />
         <Projects />
         <Skills />
+        {/* <Honers /> */}
         <Contact />
       </main>
 
--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,24 +1,49 @@
+/* eslint-disable react/no-unescaped-entities */
 import GitHubIcon from '@material-ui/icons/GitHub'
 import LinkedInIcon from '@material-ui/icons/LinkedIn'
-import LogoDevIcon from '@mui/icons-material/LogoDev';
+import LogoDevIcon from '@mui/icons-material/LogoDev'
 import { about } from '../../portfolio'
 import './About.css'
 
 const About = () => {
-  const { name, role, description, social } = about
+  const { name, role, social } = about
 
   return (
     <div className='about center'>
       {name && (
         <h1>
-          Hi, I am <span className='about__name'>{name}</span>
+          Hi, I am <span className='about__name'>{name}</span>{' '}
+          {/* <img
+            src='https://em-content.zobj.net/source/telegram/386/man-technologist_1f468-200d-1f4bb.webp'
+            alt=''
+            height='90px'
+          /> */}
         </h1>
       )}
-      
-
       {role && <h2 className='about__role'>{`<> A ${role} </>`}</h2>}
-      <p className='about__desc'>{description && description}</p>
-
+      🚀 Passionate React.js/Node.js Full Stack Developer 🚀
+      <p className='about__desc'>
+        <div>
+          {' '}
+          🎯 As a dedicated Full Stack JavaScript Developer, I specialize in
+          crafting innovative solutions with React.js and Node.js.{' '}
+        </div>{' '}
+        <br />
+        <div>
+          {' '}
+          🎯With a keen eye for detail and a knack for problem-solving, I craft
+          robust and scalable web applications that deliver exceptional user
+          experiences.{' '}
+        </div>
+        <br />
+        <div>
+          {' '}
+          🎯 With a passion for innovation, I deliver robust solutions that meet
+          the demands of modern web development while ensuring an exceptional
+          user experience.{' '}
+        </div>
+        <br />
+      </p>
       <div className='about__contact center'>
         {/* {resume && (
           <a href={resume}>
